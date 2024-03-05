8cc7dda1f4ea66dc34cf161c710a40d4523c90b5
semenInRussia <hrams205@gmail.com>
2024-03-05 17:03:51 +0300

    fish: change greeting function

---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,10 @@
+function fish_greeting
+    set_color --italics --bold
+    printf 'Yet another '
+    set_color brblue
+    printf 'epic session'
+    set_color normal
+    echo '!'
+    echo '-- @semenInRussia'
+    echo
+end
