a87e687a0548a41920d2c2f6f451d5b5274b951f
semenInRussia <hrams205@gmail.com>
2024-03-12 17:37:14 +0300

    nnn: use more plugins

--- .config/fish/conf.d/nnn.fish
+++ .config/fish/conf.d/nnn.fish
@@ -1,1 +1,8 @@
-set -x NNN_PLUG 'h:gitroot;s:suedit;n:bulknew;q:fixname;o:organize;u:getplugs;p:pskill;x:xdgdefault'
+# here I define keybindings and respective plugins
+#
+# to call one of them I must hit ; with plugin key followed.
+# I hit "; q"" to "fixname" of a file at the point
+set -x NNN_PLUG 'h:gitroot;s:suedit;n:bulknew;q:fixname;o:organize;u:getplugs;p:preview-tabbed;x:xdgdefault;t:telegram-send'
+
+# I don't know what is it
+set -x NNN_FIFO /tmp/nnn.fifo
